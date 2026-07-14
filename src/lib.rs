86c80afa65f442e1c22d2eb5d2fdcf21f9eb568e
JTKaduma <jasonbahago@gmail.com>
2026-07-14 22:59:21 +0100

    refactor: prepare contract string support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, String, Symbol, Vec};
 
 // Invoice status
 #[contracttype]
